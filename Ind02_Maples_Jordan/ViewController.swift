b491f673d53abe584954211a053702e82857f215
Jordan Maples <jordan.d.maples@gmail.com>
2022-02-18 00:18:21 -0600

    IBOutlet for picCells | Added Name Label

--- Ind02_Maples_Jordan/ViewController.swift
+++ Ind02_Maples_Jordan/ViewController.swift
@@ -9,9 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var picCells: [UIImageView]!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
 
 
