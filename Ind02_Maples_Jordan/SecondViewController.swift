e9dd123b93bf07178f14f1c18367d983264dc192
Jordan Maples <jordan.d.maples@gmail.com>
2022-02-17 23:42:40 -0600

    Create Buttons | Second View Controller

---
+++ Ind02_Maples_Jordan/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Ind02_Maples_Jordan
+//
+//  Created by Jordan Maples on 2/17/22.
+//
+
+import UIKit
+
+class SecondViewController: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
