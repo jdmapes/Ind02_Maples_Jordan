d3e4da04780e7062f6980bba618c52c226cb54fc
Jordan Maples <jordan.d.maples@gmail.com>
2022-02-20 15:08:25 -0600

    recognized potential fail point in arithmetic  for relative distance | point precision

--- Ind02_Maples_Jordan/ViewController.swift
+++ Ind02_Maples_Jordan/ViewController.swift
@@ -49,10 +49,66 @@ class ViewController: UIViewController {
         let reldis_yMax = 1.29     // Maximum relative distance between adjacent images.y coords
         var count = 1
         
-        var upOrRight = Bool.random()    // Starts the blank image swapping up or Right
-        print(upOrRight)
-        
+        randBool()
+//        print("blank location ", picCells[16].center, " ", "right image location", picCells[17].center, " ", "top image location", picCells[13].center)
+
         while (count > 0) {
+
+            var randomInt = Int.random(in: 0...19)      // Random image number to check if adjacent
+
+            var relativeDisofTwoImg = relativeDis(blankImage: blank, image: picCells[16])
+
+            var relDisOfImg_x = relativeDisofTwoImg.0
+            var relDisOfImg_y = relativeDisofTwoImg.1
+            print("Relative dis of img_x ", relDisOfImg_x, " Relative dis of img_y ", relDisOfImg_y)
+            // **** Will need a case for diagonal****//
+
+//            if (relDisOfImg_x < reldis_xMin || relDisOfImg_x > reldis_xMax) {
+//                return
+//            } else if (relDisOfImg_y < reldis_yMin || relDisOfImg_y > reldis_yMax) {
+//                return
+//            } else {
+//                //Swap Function
+//                let result = swaptest(blank: blank, image: picCells[randomInt])
+//
+//            }
+
+            count -= 1
+        }
+
+        
+        // Relative difference is calculated by taking [(newVal + reference) - reference]/ reference
+        // Relative difference between blank image and comparison image, if falls in range, swap
+        func relativeDis(blankImage: UIImageView, image: UIImageView) -> (Double, Double) {
+            print("Called relativeDis function")            //FIX!!!
+
+            let blank_x = Double(blankImage.center.x)
+            let blank_y = Double(blankImage.center.y)
+
+            let _a = Double(image.center.x)
+            let _b = Double(image.center.y)
+
+            let relDisofImage_x = Double(((blank_x + _a) - blank_x) / blank_x)
+            let relDisofImage_y = Double(((blank_y + _b) - blank_y) / blank_y)
+
+            return (relDisofImage_x, relDisofImage_y)
+        }
+//
+//
+//        func swaptest(blank: UIImageView, image: UIImageView) -> (CGPoint, CGPoint) {
+//            // If statement checking position of next location
+//            let temp = blank.center
+//            blank.center = image.center
+//            image.center = temp
+//            return(picCells[16].center, picCells[17].center)
+//        }
+        func randBool() {
+            var upOrRight = Bool.random()
+            
+            // Starts the blank image swapping up or Right
+            print(upOrRight)
+        
+            // If statement will only execute the first loop
             //First Swap
             if upOrRight {
                 //Swap with 17
@@ -67,59 +123,8 @@ class ViewController: UIViewController {
                 picCells[12].center = temp
                 print("Swapped with 13")
             }
-            
-//            var randomInt = Int.random(in: 0...19)      // Random image number to check if adjacent
-//
-//            var relativeDisofTwoImg = relativeDis(blankImage: blank, image: picCells[16])
-//
-//            var relDisOfImg_x = relativeDisofTwoImg.0
-//            var relDisOfImg_y = relativeDisofTwoImg.1
-            
-            //**** Will need a case for diagonal****//
-            
-//            if (relDisOfImg_x < reldis_xMin || relDisOfImg_x > reldis_xMax) {
-//                break
-//            } else if (relDisOfImg_y < reldis_yMin || relDisOfImg_y > reldis_yMax) {
-//                break
-//            } else {
-//                //Swap Function
-//                let result = swaptest(blank: blank, image: picCells[randomInt])
-//
-//
-//
-//                       picCells[17].center = result.1
-//                       picCells[16].center = result.0
-//            }
-//
-            count -= 1
-//        }
+        }
         
-        
-        // Relative difference is calculated by taking [(newVal + reference) - reference]/ reference
-        // Relative difference between blank image and comparison image, if falls in range, swap
-//        func relativeDis(blankImage: UIImageView, image: UIImageView) -> (Double, Double) {
-//            print("Called relativeDis function")            //FIX!!!
-//
-//            let blank_x = Double(blankImage.center.x)
-//            let blank_y = Double(blankImage.center.y)
-//
-//            let _a = Double(image.center.x)
-//            let _b = Double(image.center.y)
-//
-//            let relDisofImage_x = (blank_x + _a) - blank_x / blank_x
-//            let relDisofImage_y = (blank_y + _b) - blank_y / blank_y
-//
-//            return (relDisofImage_x, relDisofImage_y)
-//        }
-//
-        
-//        func swaptest(blank: UIImageView, image: UIImageView) -> (CGPoint, CGPoint) {
-//            // If statement checking position of next location
-//            let temp = picCells[16].center
-//            picCells[16].center = picCells[17].center
-//            picCells[17].center = temp
-//            return(picCells[16].center, picCells[17].center)
-//        }
         
         
 //        let result = swaptest()
@@ -138,11 +143,6 @@ class ViewController: UIViewController {
 //        print(picCells[16].center, " ", picCells[17].center)
     }
     
-    
-   
-        
-    
-        
 }
     
     
@@ -160,4 +160,4 @@ class ViewController: UIViewController {
 //    }
     
 
-}
+
