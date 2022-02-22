57cf31412c18e65b7c19217c2b31f7aabdc33595
Jordan Maples <jordan.d.maples@gmail.com>
2022-02-21 23:02:31 -0600

    Created Tags for Each Image

--- Ind02_Maples_Jordan/ViewController.swift
+++ Ind02_Maples_Jordan/ViewController.swift
@@ -17,11 +17,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setCellTag()
-        //print(picCenters.count)  //FIXME!!! Remove before final submission
         print(image_centers)
         print(picCells[16].center) // Prints IBOutlet elements center
         print(image_centers[18]!) // Prints certain keys value
-//      print(picCells[16].center.y) // Prints y coordinate of image
     }
 
     // Dictionary of [Position of Image : CenterPoint for that Image]
@@ -39,14 +37,10 @@ class ViewController: UIViewController {
     // Shuffle Action Jackson
     @IBAction func shuffleTapped(_ sender: UITapGestureRecognizer) {
         
-        print("Shuffle Tapped")     //FIXME!!!!
-        
         let blank = picCells[16]
         
         var count = 25
         var counter = 0
-//        randBool()
-//        print("blank location ", picCells[16].center, " ", "right image location", picCells[17].center, " ", "top image location", picCells[13].center)
 
         while (count > 0) {
             
@@ -85,65 +79,9 @@ class ViewController: UIViewController {
             return()
         }
         
-//
-//
-//        func swaptest(blank: UIImageView, image: UIImageView) -> (CGPoint, CGPoint) {
-//            // If statement checking position of next location
-//            let temp = blank.center
-//            blank.center = image.center
-//            image.center = temp
-//            return(picCells[16].center, picCells[17].center)
-//        }
-//        func randBool() {
-//            var upOrRight = Bool.random()
-//
-//            // Starts the blank image swapping up or Right
-//            print(upOrRight)
-//
-//            // If statement will only execute the first loop
-//            //First Swap
-//            if upOrRight {
-//                //Swap with 17
-//                let temp = blank.center
-//                blank.center = picCells[17].center
-//                picCells[17].center = temp
-//                print("Swapped with 17")
-//            } else {
-//                //Swap with 12
-//                let temp = blank.center
-//                blank.center = picCells[12].center
-//                picCells[12].center = temp
-//                print("Swapped with 13")
-//            }
-//        }
-        
-        
-        
-//        let result = swaptest()
-//
-        
-//
-//        picCells[17].center = result.1
-//        picCells[16].center = result.0
-//
-//        print(picCells[16].center, " ", picCells[17].center)
     }
     
+    
 }
-    
-    
-    
-    
-    // Create Objects of class type cells with a position and it's center
-//    class picCenter {
-//        var origPos: Int
-//        var x_y: CGPoint
-//
-//        init(pos: Int, X_Y: CGPoint) {
-//            self.origPos = pos
-//            self.x_y = X_Y
-//        }
-//    }
-    
 
 
